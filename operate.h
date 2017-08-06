109cfdbcd65172f641e23aab97e1033362efdcc3
Dmitriy <dm.zelenets@gmail.com>
2017-08-07 00:48:33 +0300

    Viginere cipher implementation

--- operate.h
+++ operate.h
@@ -7,10 +7,8 @@
 
 #include <stdio.h>
 
-
-
-char* check(char*, int*);
-void encode(char* , FILE*);
-void decode(char* , FILE*);
+char* check(char*, char*, int*);
+void encode(char*, char*, FILE*);
+void decode(char*, char*, FILE*);
 
 #endif //VGNR_OPERATE_H
